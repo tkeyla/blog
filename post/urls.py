0e73d0d9451f2deb1eca4736c22d436f1408ecba
keyla <ktacuabe@udc.edu.ar>
2024-09-23 17:19:29 -0300

    Se agregaron las funciones de registro, login y logout de usuarios

--- post/urls.py
+++ post/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.principal, name='home'),
+    path('accounts/signup', views.crear_usuario, name='registro')
 ]
--- post/views.py
+++ post/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .templates import *
+from django.contrib.auth.models import User
+from .forms import *
 
 def principal(request):
     return render(request, 'home.html')
+
+def crear_usuario(request):
+    if request.method == 'POST':
+        form = SignUp(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            User.objects.create_user(username, email, password)
+            return redirect('login')
+    else:
+        form = SignUp()
+        return render(request, 'registration/registro.html', {'form':form})
+
 
 
